4538849de25a131c9817e432c514218d1433c4f0
Sahil Raj <sahilraj987199@gmail.com>
2026-09-18 12:10:44 +0530

    Update db.ts

--- src/infrastructure/db.ts
+++ src/infrastructure/db.ts
@@ -15,7 +15,5 @@ export const prisma =
     log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+globalForPrisma.prisma = prisma;
 
